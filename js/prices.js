7ea79386ef6ee1e3840e95b57f975cac26a0183f
gersongahg <gahg.10gerson@gmail.com>
2025-10-11 13:55:37 -0700

    prices

---
+++ js/prices.js
@@ -0,0 +1,45 @@
+const precios = [
+    hamburguesa = {
+        uno: 7500,
+        dos: 14800,
+        unoEsp: 7800,
+        dosEsp: 15500
+    },
+    lomo = {
+        uno: 8000,
+        dos: 15800,
+        tres: 23500,
+        unoEsp: 8500,
+        dosEsp: 16500
+    },
+    pizza = {
+        muzza: 7500,
+        muzzaEsp: 8000,
+        espJaMo: 8000,
+        espHu: 8500,
+        espAn: 9000,
+        nap: 8500,
+        fug: 8500,
+        cala: 9000,
+        ofertamuzzaesp: 15000,
+        ofertamuzzaespnap: 23000
+    },
+    sanguches = {
+        ter: 8000,
+        terEsp: 8500,
+        unohamlom: 7800,
+        doshamlom: 15500,
+        treshamlom: 22000,
+        mila: 7800 
+    },
+    gigantes = {
+        ham: 28000,
+        lom: 29000,
+        pizzalom: 30000
+    },
+    empanadas = {
+        doce: 12000,
+        seis: 7000,
+        uno: 1200
+    }
+]
